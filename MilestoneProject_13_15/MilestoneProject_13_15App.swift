78b79c5417676660df97e519270512b084d07d20
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-06 11:38:04 +0800

    Initial Commit

---
+++ MilestoneProject_13_15/MilestoneProject_13_15App.swift
@@ -0,0 +1,20 @@
+//
+//  MilestoneProject_13_15App.swift
+//  MilestoneProject_13_15
+//
+//  Created by Giovanni Gaffé on 2021/12/6.
+//
+
+import SwiftUI
+
+@main
+struct MilestoneProject_13_15App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
