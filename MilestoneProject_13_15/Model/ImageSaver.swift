6da110983d5f6898917f6d72f9b4fe9c85c64658
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-06 13:38:41 +0800

    Save image

---
+++ MilestoneProject_13_15/Model/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  MilestoneProject_13_15
+//
+//  Created by Giovanni Gaffé on 2021/12/6.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
